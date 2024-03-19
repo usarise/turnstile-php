dfbc1779f99e33423c933cd78e5ee99770e7555a
usarise <7043681+usarise@users.noreply.github.com>
2024-03-20 02:37:50 +0700

    update dev dependency rector/rector to ^1.0.3

--- rector.php
+++ rector.php
@@ -2,33 +2,32 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->cacheDirectory(__DIR__ . '/var/cache/rector');
-
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withCache(
+        cacheDirectory: __DIR__ . '/var/cache/rector',
+    )
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
-    ]);
-
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-
-    $rectorConfig->skip([
+    ])
+    ->withSkip([
         NewlineAfterStatementRector::class,
-    ]);
-
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
-        SetList::DEAD_CODE,
-        SetList::CODE_QUALITY,
-        SetList::CODING_STYLE,
-        SetList::EARLY_RETURN,
-        SetList::TYPE_DECLARATION,
-        SetList::PRIVATIZATION,
-    ]);
-};
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        instanceOf: true,
+    )
+    ->withAttributesSets(
+        phpunit: true,
+    )
+    ->withPhpSets()
+    ->withRootFiles()
+;
