f38adb5fad8c4a20abb18159a0a9521a07ae661c
usarise <7043681+usarise@users.noreply.github.com>
2025-03-08 11:22:59 +0700

    add `metadata` to response for integration test

--- tests/TurnstileIntegrationTest.php
+++ tests/TurnstileIntegrationTest.php
@@ -57,11 +57,14 @@ final class TurnstileIntegrationTest extends TestCase {
                 'success' => false,
                 'error-codes' => ['invalid-input-response'],
                 'messages' => [],
+                'metadata' => [
+                    'result_with_testing_key' => true,
+                ],
             ],
             $response->toArray(strict: false),
         );
         $this->assertEquals(
-            '{"success":false,"error-codes":["invalid-input-response"],"messages":[]}',
+            '{"success":false,"error-codes":["invalid-input-response"],"messages":[],"metadata":{"result_with_testing_key":true}}',
             (string) $response,
         );
     }
